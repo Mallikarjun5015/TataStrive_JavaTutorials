ec92501705616901d4b7e84fe8ff3f0aa975f57e
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-03-11 21:20:10 +0530

    Basic Data Types in java

--- TS_Java00/src/dataTypes/Variables.java
+++ TS_Java00/src/dataTypes/Variables.java
@@ -4,11 +4,21 @@ public class Variables {
 
 	// Instance variable outside the method and inside the class
 	static int b = 20;
+	
+	String name="Dinga";
+	
+	void detail() {
+		System.out.println(b);
+		System.out.println(name);
+	}
 
 	public static void main(String[] args) {
 		// Local variable inside the method
 		int a = 10;
 		System.out.println(a);
 		System.out.println(b);
+		Variables v = new Variables();
+		v.detail();
+		
 	}
 }
