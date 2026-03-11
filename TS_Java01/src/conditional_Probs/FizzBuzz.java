ccc36a332aa4b952e92d252c66af5e7ae177e279
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-03-11 21:21:39 +0530

    Conditional problems in java

--- TS_Java01/src/conditional_Probs/FizzBuzz.java
+++ TS_Java01/src/conditional_Probs/FizzBuzz.java
@@ -6,30 +6,19 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        //a%3==0 && a%5==0
-        if(a%3==0 ) {
-        	System.out.println("Fizz");
-        }else if(a%5==0){
-        	System.out.println("Buzz");
-        }else
-        	
-        	
-        	//here 
-        	if(a%3==0 && a%5==0){
-        		
-        		 if(a%3==0 ) {
-        	        	System.out.println("Fizz");
-        	        }else if(){
-        	        	System.out.println("Buzz");
-        	        }
-        	System.out.println("FizzBuzz");
-        }
-        else {
-        	System.out.println(7);
-        }
+		int a = sc.nextInt();
+		// here
+		if (a % 3 == 0 && a % 5 == 0) {
+			System.out.println("FizzBuzz");
+		} else if (a % 3 == 0) {
+			System.out.println("Fizz");
+		} else if (a % 5 == 0) {
+			System.out.println("Buzz");
+		} else {
+			System.out.println(a);
+		}
 	}
 
 }
