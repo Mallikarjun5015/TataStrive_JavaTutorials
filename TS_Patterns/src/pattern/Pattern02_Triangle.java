aa73910bdc2def0f25ace109f78afc0f515dc0e5
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-03-13 23:59:09 +0530

    Triangle patterns

--- TS_Patterns/src/pattern/Pattern02_Triangle.java
+++ TS_Patterns/src/pattern/Pattern02_Triangle.java
@@ -146,6 +146,34 @@ public class Pattern02_Triangle {
 		}
 		
 		
+      for (int i=1; i<=rows;i++)
+      {
+          for(int j=1;j<=i;j++)
+          {
+              if( j==1 || i==j || i==rows)
+              {
+                  System.out.print("*"+" ");
+              }
+              else
+              {
+                  System.out.print(" "+" ");
+              }
+          }
+          System.out.println();
+      }
+
+      for (int i=rows; i>=1;i--)
+		{
+			for (int j = 1; j <= i; j++) {
+				if (j == 1 || i == j || i == rows) {
+					System.out.print("*" + " ");
+				} else {
+					System.out.print(" " + " ");
+				}
+			}
+			System.out.println();
+		}
+
 		
 		
 		
