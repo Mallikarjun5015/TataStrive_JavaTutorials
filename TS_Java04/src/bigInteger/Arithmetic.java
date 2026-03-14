76a0b1dc20ac3ab35af34d9b9017dc59f7396a07
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-03-14 13:37:20 +0530

    BigInteger in Java

---
+++ TS_Java04/src/bigInteger/Arithmetic.java
@@ -0,0 +1,14 @@
+package bigInteger;
+
+import java.math.BigInteger;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		
+		BigInteger a = new BigInteger("12345");
+		System.out.println("The value of a is: "+a);
+
+	}
+
+}
---
+++ TS_Java04/src/bigInteger/BigIntJava.java
@@ -0,0 +1,29 @@
+package bigInteger;
+
+import java.math.BigInteger;
+
+public class BigIntJava {
+
+	public static void main(String[] args) {
+		
+		// Initializing BigInteger in Java
+		
+		// 01. Using New Keyword
+		
+		BigInteger a = new BigInteger("12345");
+		System.out.println("The value of a is: "+a);
+		
+		// 02. Passing String Variable
+		
+		String b = "43215";
+		a=new BigInteger(b);
+		System.out.println("The value of a is: "+a);
+		
+		// 03. Using valueOf() static method 
+		
+		a=BigInteger.valueOf(34567);
+		System.out.println("The value of a is: "+a);
+
+	}
+
+}
