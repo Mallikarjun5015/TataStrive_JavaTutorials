7f14935a3f63e489059be54c7bff8ce853335bc8
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-03-17 15:35:51 +0530

    Nested exception in Java.

--- TS_Java04/src/exception/Exception_03.java
+++ TS_Java04/src/exception/Exception_03.java
@@ -3,19 +3,53 @@ package exception;
 public class Exception_03 {
 
 	public static void main(String[] args) {
-		
+
 		String name = null;
-		
+
 		try {
-			
-			//System.out.println(name[2]);
-			
+
+			try {
+				int a = 10 / 0; // This causes ArithmeticException
+			} catch (ArithmeticException e) {
+				System.out.println("Inner catch: " + e);
+			}
+
 			System.out.println(name.length());
 		} catch (Exception e) {
-			
+
 			System.out.println(e);
 		}
 		
-		}
+		try {
+            // Outer try block
+            System.out.println("Outer try block started");
+
+            // Inner try block 1: handles ArithmeticException
+            try {
+                int n = 10;
+                int res = n / 0;  // This will throw ArithmeticException
+            } catch (ArithmeticException e) {
+                System.out.println("Caught ArithmeticException in inner try-catch: " + e);
+            }
+
+            // Inner try block 2: handles NullPointerException
+            try {
+                String s = null;
+                System.out.println(s.length());  // This will throw NullPointerException
+            } catch (NullPointerException e) {
+                System.out.println("Caught NullPointerException in inner try-catch: " + e);
+            }
+
+        } catch (Exception e) {
+            // Outer catch block (optional general handler)
+            System.out.println("Caught exception in outer try-catch: " + e);
+        } finally {
+            // Finally block executes in most cases
+            System.out.println("Finally block executed");
+        }
+
+        System.out.println("Program continues after nested try-catch");
+
+	}
 
 }
--- TS_Java04/src/exception/Exception_04.java
+++ TS_Java04/src/exception/Exception_04.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Exception_04 {
 	public static void main(String[] args) {
-		
+
 		try {
 			method();
 		} catch (ArithmeticException e) {
@@ -16,25 +16,24 @@ public class Exception_04 {
 
 		catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println(e.getMessage());
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 
 	}
 
 	public static void method() throws FileNotFoundException {
-		int a=10,b=5,c;
-		
-		c=a/b;
-		System.out.println("The value of C: "+c);
-		
-		int d[]= new int[2];
-		d[0]=d[1]=10;
-		//int e=d[2];
-		
+		int a = 10, b = 5, c;
+
+		c = a / b;
+		System.out.println("The value of C: " + c);
+
+		int d[] = new int[2];
+		d[0] = d[1] = 10;
+		// int e=d[2];
+
 		Scanner sc = new Scanner(new File("File.txt"));
-		
+
 	}
 
 }
