cca92f3668b8382f154f8cb10096e5123a16e885
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-03-18 16:09:07 +0530

    File reader and writer using Buffer and scanner class in Java.

---
+++ TS_Java05/src/fileHandling/File06.java
@@ -0,0 +1,49 @@
+package fileHandling;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class File06 {
+
+	public static void main(String[] args) throws IOException {
+	
+		File file = new File("files01.txt") ;
+		
+		FileReader read = null;
+		FileWriter wt = null;
+		
+		read = new FileReader(file);
+		wt = new FileWriter("file02.txt");
+		
+		BufferedReader bfr=null;
+		BufferedWriter bft = null;
+		
+		bfr = new BufferedReader(read);
+		bft = new BufferedWriter(wt); 
+			
+		String data;
+		
+		while((data=bfr.readLine())!=null) {
+			System.out.print(data);
+			bft.write(data);
+			bft.newLine();
+		}
+		
+		Scanner sc = new Scanner(file);
+		
+		while(sc.hasNext()) {
+			data= sc.nextLine();
+			System.out.println("\nUsing Scanner");
+			System.out.print(data);
+		}
+		
+		bft.close();
+
+	}
+
+}
