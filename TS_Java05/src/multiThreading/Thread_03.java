da6f4d583957fa8802a2ae3c9eb2d31f2236d500
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-03-18 14:57:58 +0530

    Threa methods and life cycle in Java.

--- TS_Java05/src/multiThreading/Thread_03.java
+++ TS_Java05/src/multiThreading/Thread_03.java
@@ -1,5 +1,19 @@
 package multiThreading;
 
+class MyThread implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("Runnable Thread");
+	}
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
 class Cooking extends Thread{
 	
 	private String item;
@@ -10,6 +24,12 @@ class Cooking extends Thread{
 	}
 	
 	@Override
+	public void start() {
+		super.start();
+		System.out.println("Overriden start");
+	}
+	
+	@Override
 	public void run() {
 		super.run();
 		System.out.println("The item "+this.item+" is prepared by the chef "+Thread.currentThread().getName());
@@ -17,7 +37,11 @@ class Cooking extends Thread{
 }
 public class Thread_03 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		
+		Runnable r = () -> System.out.println("Running...");
+		MyThread c = new MyThread();
+		c.start();
 		Cooking c1 = new Cooking("Pasta");
 		Cooking c2 = new Cooking("Salad");
 		Cooking c3 = new Cooking("Veg Rice");
@@ -27,7 +51,10 @@ public class Thread_03 {
 		c1.start();
 		c2.start();
 		c3.start();
+		c3.join();// main waits here
+		System.out.println("Main start again");
 		c4.start();
+		Thread.yield(); // hint to wait for same or higher prio thread
 		c5.start();
 	}
 
