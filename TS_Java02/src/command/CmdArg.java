278e3f2e9a212d45dde15f912dd9f2bb0b4d3f56
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-03-28 00:12:14 +0530

    Command line argument program

--- TS_Java02/src/command/CmdArg.java
+++ TS_Java02/src/command/CmdArg.java
@@ -2,15 +2,22 @@ package command;
 
 public class CmdArg {
 
-	public static void main(String[] args) {
+	public static void main(String[] mallu) {
 		
-		System.out.println(args[0]);
-		System.out.println(args[1]);
-		System.out.println(args[2]);
-		System.out.println(args[3]);
-		
-		System.out.println(args.length);
+		/*
+		 * System.out.println(args[0]); System.out.println(args[1]);
+		 * System.out.println(args[2]); System.out.println(args[3]);
+		 * 
+		 * System.out.println(args.length);
+		 */
 
+		if(mallu.length>0) {
+			for(String s : mallu) {
+				System.out.println(s);
+			}
+		}else {
+			System.out.println("No args..");
+		}
 	}
 
 }
