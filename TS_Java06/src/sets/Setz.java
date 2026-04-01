4c5a1f17f6f8055421d8cc1e2f7c6a690da2e16a
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-04-01 23:28:16 +0530

    Sets methods in Java

--- TS_Java06/src/sets/Setz.java
+++ TS_Java06/src/sets/Setz.java
@@ -2,6 +2,7 @@ package sets;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Setz {
 
@@ -18,9 +19,13 @@ public class Setz {
 
 	        System.out.println(s);
 	        
-	        String s1 = "D";
+	        String ss = "D";
 
-	        System.out.println("Contains " + s + " " + s.contains(s1));
+	        System.out.println("Contains " + s + " " + s.contains(ss));
+	        
+	        System.out.println("Is empty? "+s.isEmpty());
+	        
+	        System.out.println("Size ? "+s.size());
 	        
 	        s.remove("B");
 	        System.out.println("After removing element " + s);
@@ -30,6 +35,33 @@ public class Setz {
 	            System.out.print(value + ", ");
 	        
 	        System.out.println();
+	        
+	        Object[] arr = s.toArray();
+	        
+	        for (Object object : arr) {
+	        	System.out.print(object+" ");
+			}
+	        
+	        s.clear();
+	        
+	        System.out.println("\nAfter clearing "+s);
+	        
+	        Set<String> s1 = new TreeSet<String>(); 
+
+			s1.add("A"); 
+			s1.add("B"); 
+			s1.add("C"); 
+			 
+			System.out.println("Initial Set: " + s1); 
+
+			Set<String> s2 = new TreeSet<String>(); 
+
+			s2.add("D"); 
+			s2.add("E"); 
+
+			s1.addAll(s2); 
+
+			System.out.println("Final Set: " + s1); 
 
 	}
 
