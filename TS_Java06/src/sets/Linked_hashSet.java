1f473e33b7c3e2b94cb27aa275a30c7ac1460382
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-04-03 22:57:18 +0530

    Basic operation using LinkedHashSet

--- TS_Java06/src/sets/Linked_hashSet.java
+++ TS_Java06/src/sets/Linked_hashSet.java
@@ -5,28 +5,44 @@ import java.util.LinkedHashSet;
 public class Linked_hashSet {
 
 	public static void main(String[] args) {
-		
+
 		LinkedHashSet<Integer> num = new LinkedHashSet<Integer>();
-		
+
 		num.add(15);
 		num.add(25);
 		num.add(5);
 		num.add(10);
 		num.add(20);
 		System.out.println(num);
-		
+
 		num.add(30);
 		System.out.println(num);
-		
+
 		num.addFirst(0);
 		num.addLast(40);
 		System.out.println(num);
-		
+
 		num.remove(5);
 		System.out.println(num);
+
+		System.out.println(num.getFirst() + " first");
+		System.out.println(num.getLast() + " last");
+
+		LinkedHashSet<String> set = new LinkedHashSet<>();
+		set.add("Apple");
+		set.add("Banana");
+		set.add("Cherry");
+		set.add("Apple");
+		System.out.println("" + set);
 		
-		System.out.println(num.getFirst()+" first");
-		System.out.println(num.getLast()+" last");
+		set.add("Grapes");
+		set.add("Mango");
+		
+		System.out.println("" + set);
+		
+		set.remove("Mango");
+		
+		System.out.println("After removing element " + set);
 		
 		
 
