d0d8295594397747acdb54b60834ee27e072c786
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-04-04 17:22:30 +0530

    Tree set methods in java

--- TS_Java06/src/sets/Tree_Set.java
+++ TS_Java06/src/sets/Tree_Set.java
@@ -8,8 +8,7 @@ public class Tree_Set {
 	public static void main(String[] args) {
 
 		TreeSet<Integer> num = new TreeSet<Integer>();
-		
-		
+
 		num.add(15);
 		num.add(25);
 		num.add(5);
@@ -20,31 +19,44 @@ public class Tree_Set {
 		num.add(0);
 		num.add(30);
 		System.out.println(num);
-		
+
 		num.remove(20);
 		System.out.println(num);
-		System.out.println(num.first()+" first");
-		System.out.println(num.last()+" last");
+		System.out.println(num.first() + " first");
+		System.out.println(num.last() + " last");
 		
-		System.out.println(num.ceiling(18)+" > or = : ceiling");
-		System.out.println(num.floor(18)+" < or == : floor");
+		num.pollFirst();
+		System.out.println("After removing first "+num);
 		
-		System.out.println(num.higher(18)+" > : higher");
-		System.out.println(num.lower(18)+" < : lower");
-		
+		num.pollLast();
+		System.out.println("After removing last "+num);
+
+		System.out.println(num.ceiling(18) + " > or = : ceiling");
+		System.out.println(num.floor(18) + " < or == : floor");
+
+		System.out.println(num.higher(18) + " > : higher");
+		System.out.println(num.lower(18) + " < : lower");
+
 		Set<String> ts = new TreeSet<>();
 
-        
-        ts.add("Dinga");
-        ts.add("Subba");
-        ts.add("Gunda");
-        ts.add("Munda");
-        ts.add("Banda");
-        ts.add("Subba");
-        //ts.add(null);
-        
-        
-        System.out.println(ts);	
+		ts.add("Dinga");
+		ts.add("Subba");
+		ts.add("Gunda");
+		ts.add("Munda");
+		ts.add("Banda");
+		ts.add("Subba");
+		// ts.add(null);
+
+		System.out.println(ts);
+
+		String s = "Munda";
+
+		System.out.println("The Treeset contains " + s + " ? " + ts.contains(s));
+		
+		for (String string : ts) {
+			System.out.print(string+" ");
+		}
+		
 	}
 
 }
