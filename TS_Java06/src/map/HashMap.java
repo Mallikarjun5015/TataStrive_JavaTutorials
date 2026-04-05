1fb3487b3016b5fe89bf24efb8a907eb2f75c54e
Mallikarjun5015 <mallikarjunjamadar0301@gmail.com>
2026-04-06 00:01:08 +0530

    Map interface in java

--- TS_Java06/src/map/HashMap.java
+++ TS_Java06/src/map/HashMap.java
@@ -8,9 +8,26 @@ public class HashMap {
 
 		Map<String, Integer> m = new java.util.HashMap<String, Integer>();
 
-		m.put("Geek1", 1);
-		m.put("Geek2", 2);
-		m.put("Geek3", 3);
+		m.put("Gunda", 1);
+		m.put("Gundi", 2);
+		m.put("Manga", 3);
+
+		Map<String, Integer> hm1 = new java.util.HashMap<String, Integer>();
+
+		
+		Map<String, Integer> hm2 = new java.util.HashMap<String, Integer>();
+
+		hm1.put("Pinda", 1);
+		hm1.put("RCB", 2);
+		hm1.put("Virat", 3);
+
+		
+		hm2.put("Maka", 1);
+		hm2.put("For", 2);
+		hm2.put("waste", 3);
+
+		System.out.println(hm1);
+		System.out.println(hm2);
 
 	}
 
